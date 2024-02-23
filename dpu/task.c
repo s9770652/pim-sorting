68813cb117f0bedc8e4dc104a4b41d6896241706
s9770652 <s9770652@rz.uni-frankfurt.de>
2024-02-24 00:23:54 +0100

    Improved `main` function (`task.c`) and its output.

--- dpu/task.c
+++ dpu/task.c
@@ -15,8 +15,6 @@
 
 // maximum number of elements loaded into MRAM (size must be divisible by 8)
 #define LOAD_INTO_MRAM ((1024 * 1024 * 25) >> DIV)
-#define PERF 1
-#define CHECK_SANITY 1
 
 __host struct dpu_arguments DPU_INPUT_ARGUMENTS;
 T __mram_noinit input[LOAD_INTO_MRAM];  // array of random numbers
@@ -24,28 +22,69 @@ T __mram_noinit output[LOAD_INTO_MRAM];
 mram_range ranges[NR_TASKLETS];
 static struct xorshift rngs[NR_TASKLETS];  // Contains the seed for each tasklet.
 bool flipped;
+
+BARRIER_INIT(omni_barrier, NR_TASKLETS);
+
 #if PERF
 perfcounter_t cycles[NR_TASKLETS];  // Used to measure the time for each tasklet.
 #endif
+
 #if CHECK_SANITY
-bool sorted;
+bool same_elems, sorted;
 MUTEX_INIT(sorted_mutex);
 #endif
 
-BARRIER_INIT(omni_barrier, NR_TASKLETS);
 
 inline size_t align(size_t to_align) {
     return ROUND_UP_POW2(to_align << DIV, 8) >> DIV;
 }
 
+/**
+ * @brief Calculates the sum of a given MRAM array
+ * and also prints its content if the length is at most 2048.
+ * For this reason, this function is currently sequential.
+ * @param array The MRAM array whose sum is to be calculated.
+ * @param cache A cache in WRAM.
+ * @param counts Array of occurrences of each array element smaller than 8.
+ * Should be initialised to all zeroes.
+ * @param length The length of the MRAM array.
+ * @param label The text to be shown before the array is printed.
+ * @return The sum of all elements in `array`.
+ */
+uint64_t get_sum(T __mram_ptr *array, T *cache, size_t counts[8], size_t const length, char *label) {
+    if (me() != 0) return 0;
+    size_t max_length_to_print = 2048;
+    if (length <= max_length_to_print) {
+        printf("%s", label);
+    }
+    uint64_t sum = 0;
+    size_t i, curr_length, curr_size;
+    mram_range sum_range = { 0, length };
+    LOOP_ON_MRAM(i, curr_length, curr_size, sum_range) {
+        mram_read(&array[i], cache, curr_size);
+        if (length <= max_length_to_print) {
+            print_array(cache, curr_length);
+        }
+        for (size_t x = 0; x < curr_length; x++) {
+            sum += cache[x];
+            if (cache[x] < 8)
+                counts[cache[x]]++;
+        }
+    }
+    if (length <= max_length_to_print) {
+        printf("\n");
+    }
+    return sum;
+}
+
 int main() {
     if (me() == 0) {
         mem_reset();
-#if PERF
-        perfcounter_config(COUNT_CYCLES, true);
-#endif
 #if CHECK_SANITY
         sorted = true;
+#endif
+#if PERF
+        perfcounter_config(COUNT_CYCLES, true);
 #endif
         printf("input length: %d\n", DPU_INPUT_ARGUMENTS.length);
         printf("diff to max length: %d\n", LOAD_INTO_MRAM - DPU_INPUT_ARGUMENTS.length);
@@ -100,23 +139,8 @@ int main() {
 #endif
 
 #if CHECK_SANITY
-    uint64_t sum = 0;
-    size_t cnts[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    if (me() == 0) {
-        printf("Before sorting:\n");
-        mram_range testrange = { 0, length };
-        LOOP_ON_MRAM(i, curr_length, curr_size, testrange) {
-            mram_read(&input[i], cache, curr_size);
-            if (length <= 2048)
-                print_array(cache, curr_length);
-            for (size_t x = 0; x < curr_length; x++) {
-                sum += cache[x];
-                if (cache[x] < 8)
-                    cnts[cache[x]]++;
-            }
-        }
-        printf("\n");
-    }
+    size_t counts_1[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    uint64_t sum_1 = get_sum(input, cache, counts_1, length, "Before sorting:\n");
     barrier_wait(&omni_barrier);
 #endif
 
@@ -139,35 +163,26 @@ int main() {
 #endif
 
 #if CHECK_SANITY
-    uint64_t sumo = 0;
-    size_t cntso[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    /* Check if the numbers stayed the same. */
+    size_t counts_2[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    uint64_t sum_2 = get_sum(within, cache, counts_2, length, "After sorting:\n");
     if (me() == 0) {
-        printf("After sorting:\n");
-        mram_range testrange = { 0, length };
-        LOOP_ON_MRAM(i, curr_length, curr_size, testrange) {
-            mram_read(&within[i], cache, curr_size);
-            if (length <= 2048)
-                print_array(cache, curr_length);
-            for (size_t x = 0; x < curr_length; x++) {
-                sumo += cache[x];
-                if (cache[x] < 8)
-                    cntso[cache[x]]++;
+        same_elems = sum_1 == sum_2;
+        for (size_t c = 0; c < 8; c++) {
+            same_elems = same_elems && counts_1[c] == counts_2[c];
+        }
+        if (!same_elems) {
+            printf("[" ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "] Elements have changed.\n");
+            printf("\nSums: %lu ↔ %lu\nCounts: ", sum_1, sum_2);
+            for (size_t c = 0; c < 8; c++) {
+                printf("%d: %zu ↔ %zu   ", c, counts_1[c], counts_2[c]);
             }
+            printf("\n");
         }
-        printf("sums: %lu %lu\n", sum, sumo);
-        if (sum != sumo) {
-            printf("[" ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "] Sum of elements unequal.\n");
-        }
-        printf("counts: ");
-        for (size_t c = 0; c < 8; c++)
-            printf("%d: %zu %zu  ", c, cnts[c], cntso[c]);
-        printf("\n");
     }
     barrier_wait(&omni_barrier);
-#endif
 
     /* Check if numbers were correctly sorted. */
-#if CHECK_SANITY
 #if PERF
     cycles[me()] = perfcounter_get();
 #endif
@@ -191,13 +206,12 @@ int main() {
     if (me() == 0) {
         get_time(cycles, "CHECK");
     }
-    barrier_wait(&omni_barrier);
 #endif
     if (me() == 0) {
-        if (sorted) {
-            printf("[" ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "] Elements are sorted.\n");
-        } else {
+        if (!sorted) {
             printf("[" ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "] Elements are not sorted.\n");
+        } else if (same_elems) {
+            printf("[" ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "] Elements are sorted.\n");
         }
     }
 #endif
