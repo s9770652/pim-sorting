c1888de4caec46741109e8b508e0194a172274f3
s9770652 <s9770652@rz.uni-frankfurt.de>
2024-04-04 01:46:08 +0200

    Changed signature of `generate_uniform_distribution(_wram)`.

--- dpu/random_distribution.c
+++ dpu/random_distribution.c
@@ -6,15 +6,15 @@
 
 #include "random_distribution.h"
 
-void generate_uniform_distribution(T * const cache, size_t length, T const upper_bound) {
+void generate_uniform_distribution_wram(T * const start, T * const end, T const upper_bound) {
     bool const is_power_of_two = (upper_bound & (upper_bound - 1)) == 0;
     if (is_power_of_two) {
-        for (size_t j = 0; j < length; j++) {
-            cache[j] = (gen_xs(&rngs[me()]) & (upper_bound - 1));
+        for (T *t = start; t <= end; t++) {
+            *t = (gen_xs(&rngs[me()]) & (upper_bound - 1));
         }
     } else {
-        for (size_t j = 0; j < length; j++) {
-            cache[j] = rr((T)upper_bound, &rngs[me()]);
+        for (T *t = start; t <= end; t++) {
+            *t = rr((T)upper_bound, &rngs[me()]);
         }
     }
 }
@@ -23,7 +23,7 @@ void generate_uniform_distribution_mram(T __mram_ptr *array, T * const cache,
         mram_range const * const range, T const upper_bound) {
     size_t i, curr_length, curr_size;
     LOOP_ON_MRAM(i, curr_length, curr_size, (*range)) {
-        generate_uniform_distribution(cache, curr_length, upper_bound);
+        generate_uniform_distribution_wram(cache, &cache[curr_length-1], upper_bound);
         mram_write(cache, &array[i], curr_size);
     }
 }
--- dpu/random_distribution.h
+++ dpu/random_distribution.h
@@ -14,13 +14,13 @@
 extern struct xorshift rngs[NR_TASKLETS];
 
 /**
- * @brief Uniformly draws numbers and stores them in a WRAM cache.
+ * @brief Uniformly draws numbers and stores them in a WRAM array.
  * 
- * @param cache A cache in WRAM where the random numbers are stored.
- * @param length The number of numbers to draw.
+ * @param start The start of the array where to store the random data.
+ * @param length The (inclusive) end of said array.
  * @param upper_bound The upper limit (exclusive) of the range to draw from.
 **/
-void generate_uniform_distribution(T * cache, size_t length, T upper_bound);
+void generate_uniform_distribution_wram(T *start, T *end, T upper_bound);
 
 /**
  * @brief Uniformly draws numbers and stores them in an MRAM array.
@@ -30,7 +30,7 @@ void generate_uniform_distribution(T * cache, size_t length, T upper_bound);
  * @param range For which indices of the array the numbers are drawn.
  * @param upper_bound The upper limit (exclusive) of the range to draw from.
 **/
-void generate_uniform_distribution_mram(T __mram_ptr *array, T * cache, mram_range const * range,
+void generate_uniform_distribution_mram(T __mram_ptr *array, T *cache, mram_range const *range,
         T upper_bound);
 
 #endif  // _RANDOM_DISTRIBUTION_H_
