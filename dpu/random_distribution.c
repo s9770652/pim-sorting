c36438f0a752c3f15d98935efd8259ffe232c931
s9770652 <s9770652@rz.uni-frankfurt.de>
2024-06-17 00:23:01 +0200

    Fixed pointer error in generation of almost sorted sequences.

--- dpu/random_distribution.c
+++ dpu/random_distribution.c
@@ -54,6 +54,6 @@ void generate_almost_sorted_distribution_wram(T * const start, T * const end, si
     swaps = (swaps) ? : sqroot_on_dpu(n);
     for (size_t s = 0; s < swaps; s++) {
         size_t const i = rr(n - 1, &rngs[me()]);
-        swap(start[i], start[i + 1]);
+        swap(&start[i], &start[i + 1]);
     }
 }
