0609b418d96566cb91d9e806e912fab72359f91e
s9770652 <s9770652@rz.uni-frankfurt.de>
2024-07-24 00:25:33 +0200

    Automated testing on QuickSort’s threshold.

---
+++ scripts/quick_fallback.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+NR_TASKLETS=1
+BLOCK_SIZE=16512
+SEQREAD_CACHE_SIZE=1024
+PARTITION_PRIO=2
+PIVOT="MEDIAN_OF_RANDOM"
+RECURSIVE=false
+
+b=2
+r=1000
+
+cmd="NR_TASKLETS=${NR_TASKLETS} BLOCK_SIZE=${BLOCK_SIZE} SEQREAD_CACHE_SIZE=${SEQREAD_CACHE_SIZE} PIVOT=${PIVOT} PARTITION_PRIO=${PARTITION_PRIO} RECURSIVE=${RECURSIVE} make all"
+
+main_folder=scripts/quick/fallback
+mkdir -p ${main_folder}
+
+for type in 32 64
+do
+    make clean
+    eval "TYPE=UINT${type} ${cmd}"
+
+    folder=${main_folder}/uint${type}
+    mkdir -p ${folder}
+    for threshold in {11..20}
+    do
+        rm obj/benchmark/quick_sorts.o bin/quick_sorts obj/host/app.o
+        eval "QUICK_THRESHOLD=${threshold} TYPE=UINT${type} ${cmd}"
+
+        bin/host -b ${b} -r ${r} -t 3 | tee ${folder}/${threshold}.txt
+    done
+done
