3d5909e51464bfe3b35b36e3226da57a94e56986
s9770652 <s9770652@rz.uni-frankfurt.de>
2024-08-14 01:06:16 +0200

    Fixed number of repetitions in `merge_fallback.sh`.

--- scripts/merge_fallback.sh
+++ scripts/merge_fallback.sh
@@ -5,7 +5,7 @@ BLOCK_SIZE=50000
 SEQREAD_CACHE_SIZE=1024
 
 b=3
-r=1
+r=1000
 n=""
 
 main_folder=scripts/merge
@@ -25,12 +25,12 @@ for type in 32 64
 do
     make clean
     NR_TASKLETS=${NR_TASKLETS} BLOCK_SIZE=${BLOCK_SIZE} SEQREAD_CACHE_SIZE=${SEQREAD_CACHE_SIZE} TYPE=UINT${type} make all
-    for threshold in 16 17 18 24 32 48 64 96
+    for threshold in 12 13 14 15 16 17 18 24 32 48 64 96
     do
         folder=${main_folder}/threshold=${threshold}/uint${type}
         mkdir -p ${folder}
 
-        rm obj/benchmark/wram_sorts.o bin/wram_sorts obj/host/app.o
+        rm obj/benchmark/merge_sorts.o bin/merge_sorts obj/host/app.o
         NR_TASKLETS=${NR_TASKLETS} BLOCK_SIZE=${BLOCK_SIZE} SEQREAD_CACHE_SIZE=${SEQREAD_CACHE_SIZE} TYPE=UINT${type} MERGE_THRESHOLD=${threshold} make all
 
         get_n ${threshold}
