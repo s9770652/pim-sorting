9dcd1566ee68ad216dc8e8c2382835e371efa1d3
s9770652 <s9770652@rz.uni-frankfurt.de>
2024-08-18 03:02:40 +0200

    Added first version of a sequential MRAM MergeSort.

---
+++ bench_mram/merge_mram.c
@@ -0,0 +1,230 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <alloc.h>
+#include <defs.h>
+#include <perfcounter.h>
+
+#include "wram_sorts.h"
+#include "buffers.h"
+#include "checkers.h"
+#include "common.h"
+#include "communication.h"
+#include "random_distribution.h"
+#include "random_generator.h"
+
+struct dpu_arguments __host host_to_dpu;
+struct dpu_results __host dpu_to_host;
+T __mram_noinit_keep input[LOAD_INTO_MRAM];  // set by the host
+T __mram_noinit_keep output[LOAD_INTO_MRAM];
+
+triple_buffers buffers[NR_TASKLETS];
+struct xorshift input_rngs[NR_TASKLETS];  // RNG state for generating the input (in debug mode)
+struct xorshift_offset pivot_rngs[NR_TASKLETS];  // RNG state for choosing the pivot
+
+#define STARTING_RUN_LENGTH (768)
+#define STARTING_RUN_SIZE (STARTING_RUN_LENGTH * sizeof(T))
+static_assert(!(STARTING_RUN_SIZE % 8), "The size of starting runs must be aligned on 8 bytes!");
+static_assert(
+    STARTING_RUN_SIZE <= TRIPLE_BUFFER_SIZE,
+    "The starting runs are sorted entirely in WRAM and, thus, must fit in there!"
+);
+
+// todo: flush from back to front such that the last ones in the cache can be kept
+static inline void flush_starting_run(T __mram_ptr *in, T __mram_ptr *until, T __mram_ptr *out) {
+    T * const cache = buffers[me()].cache;
+    mram_range_ptr range = { in, until + 1 };
+    T __mram_ptr *i;
+    size_t curr_length, curr_size;
+    LOOP_ON_MRAM_BL(i, curr_length, curr_size, range, STARTING_RUN_LENGTH) {
+        mram_read_triple(i, cache, curr_size);
+        mram_write_triple(cache, out, curr_size);
+        out += curr_length;
+    }
+}
+
+// todo: does inlining affect performance?
+static void flush_second(T __mram_ptr *out, T *ptr, size_t i) {
+    T * const cache = buffers[me()].cache;
+#ifdef UINT32
+    if (i & 1) {  // Is there need for alignment?
+        cache[i++] = *ptr++;  // Possible since `ptr` must have at least one element.
+    }
+#endif
+    mram_write(cache, out, i << DIV);
+}
+
+// Inlining actually worsens performance. // todo: really?
+static __noinline void flush_first(T __mram_ptr *in, T __mram_ptr *out, T *ptr,
+        size_t i, T __mram_ptr const *end) {
+    T * const cache = buffers[me()].cache;
+    // Transfer cache to MRAM.
+#ifdef UINT32
+    if (i & 1) {  // Is there need for alignment?
+        cache[i++] = *ptr++;  // Possible since `ptr` must have at least one element.
+        if (++in > end) {
+            mram_write(cache, out, i << DIV);
+            return;
+        }
+    }
+#endif
+    mram_write(cache, out, i << DIV);
+    out += i;
+
+    // Transfer from MRAM to MRAM.
+    do {
+        // Thanks to the dummy values, even for numbers smaller than 8 bytes,
+        // there is no need to round the size up.
+        size_t rem_size = (in + MAX_TRANSFER_LENGTH_CACHE > end) ? (size_t)end - (size_t)in + 4 : MAX_TRANSFER_SIZE_CACHE;
+        mram_read(in, cache, rem_size);
+        mram_write(cache, out, rem_size);
+        in += MAX_TRANSFER_LENGTH_CACHE;  // Value may be wrong for the last transfer …
+        out += MAX_TRANSFER_LENGTH_CACHE;  // … after which it is not needed anymore, however.
+    } while (in <= end);
+}
+
+static inline void merge_half_space(T __mram_ptr *out,
+        T __mram_ptr * const ends[2], seqreader_t sr[2], T *ptr[2], size_t elems_left[2]) {
+    T * const cache = buffers[me()].cache;
+    size_t i = 0;
+    while (true) {
+        if (*ptr[0] <= *ptr[1]) {
+            cache[i++] = *ptr[0];
+            ptr[0] = seqread_get(ptr[0], sizeof(T), &sr[0]);
+            if (--elems_left[0] == 0) {
+                flush_second(out, ptr[1], i);
+                break;
+            }
+        } else {
+            cache[i++] = *ptr[1];
+            ptr[1] = seqread_get(ptr[1], sizeof(T), &sr[1]);
+            if (--elems_left[1] == 0) {
+                flush_first(seqread_tell(ptr[0], &sr[0]), out, ptr[0], i, ends[0]);
+                break;
+            }
+        }
+        // If the cache is full, write its content to `output`.
+        if (i == MAX_TRANSFER_LENGTH_CACHE) {
+            mram_write(cache, out, MAX_TRANSFER_SIZE_CACHE);
+            i = 0;
+            out += MAX_TRANSFER_LENGTH_CACHE;
+        }
+    }
+}
+
+static void merge_sort(T __mram_ptr * const start, T __mram_ptr * const end) {
+    T * const cache = buffers[me()].cache;
+    mram_range_ptr range = { start, end + 1 };
+    /* Starting runs. */
+    T __mram_ptr *i;
+    size_t curr_length, curr_size;
+
+    size_t block_length = STARTING_RUN_LENGTH;
+    for (
+        curr_length = ((intptr_t)(range.end - (block_length)) >= (intptr_t)range.start) ? (block_length) : range.end - range.start,
+        curr_size = ((intptr_t)(range.end - (block_length)) >= (intptr_t)range.start) ? (block_length) << DIV : ROUND_UP_POW2(curr_length << DIV, 8),
+        i = ((intptr_t)(range.end - (block_length)) >= (intptr_t)range.start) ? range.end - (block_length) : start;
+
+        (intptr_t)curr_length > 0;
+
+        curr_length = ((intptr_t)(i - (block_length)) >= (intptr_t)range.start) ? (block_length) : i - range.start,
+        curr_size = ((intptr_t)(i - (block_length)) >= (intptr_t)range.start) ? (block_length) << DIV : ROUND_UP_POW2(curr_length << DIV, 8),
+        i = ((intptr_t)(i - (block_length)) >= (intptr_t)range.start) ? i - (block_length) : range.start
+    ) {
+        mram_read_triple(i, cache, curr_size); // todo: erstetzbar durch mram_read
+        quick_sort_wram(cache, cache + curr_length - 1);
+        mram_write_triple(cache, i, curr_size);
+    }
+
+    /* Merging. */
+    seqreader_t sr[2];
+    size_t const n = end - start + 1;
+    for (size_t run_length = STARTING_RUN_LENGTH; run_length < n; run_length *= 2) {
+        // Merge pairs of adjacent runs.
+        for (T __mram_ptr *i = end; i > start; i -= 2 * run_length) {
+            // Only one run left?
+            T __mram_ptr * const run_1_end = i - run_length;
+            if ((intptr_t)run_1_end < (intptr_t)start) {
+                break;
+            }
+            // If not, copy the current run …
+            T __mram_ptr *run_1_start = ((intptr_t)(run_1_end - run_length + 1) > (intptr_t)start) ? run_1_end - run_length + 1 : start;  // todo: use if with different flushing like with seq. MS
+            flush_starting_run(run_1_start, run_1_end, output);
+            // … and merge the copy with the next run.
+            T __mram_ptr * const ends[2] = { output + (run_1_end - run_1_start), i };
+            T *ptr[2] = {
+                seqread_init(buffers[me()].seq_1, output, &sr[0]),
+                seqread_init(buffers[me()].seq_2, run_1_end + 1, &sr[1])  // todo: wiederverwenden als ersten
+            };
+            size_t elems_left[2] = { run_1_end - run_1_start + 1, run_length };
+            merge_half_space(
+                run_1_start,
+                ends,
+                sr,
+                ptr,
+                elems_left
+            );
+        }
+    }
+}
+
+union algo_to_test __host algos[] = {
+    {{ "MergeHalfSpace", NULL }}
+};
+size_t __host num_of_algos = sizeof algos / sizeof algos[0];
+
+int main(void) {
+    if (me() != 0) return EXIT_SUCCESS;
+
+    /* Set up buffers. */
+    if (buffers[me()].cache == NULL) {  // Only allocate on the first launch.
+        allocate_triple_buffer(&buffers[me()]);
+    }
+    T * const cache = buffers[me()].cache;
+
+    /* Set up dummy values if called via debugger. */
+    if (host_to_dpu.length == 0) {
+        host_to_dpu.reps = 1;
+        host_to_dpu.length = 1024;
+        host_to_dpu.offset = ROUND_UP_POW2(host_to_dpu.length * sizeof(T), 8) / sizeof(T);
+        host_to_dpu.basic_seed = 0b1011100111010;
+        host_to_dpu.algo_index = 0;
+        input_rngs[me()] = seed_xs(host_to_dpu.basic_seed + me());
+        mram_range range = { 0, host_to_dpu.length * host_to_dpu.reps };
+        generate_uniform_distribution_mram(input, cache, &range, 8);
+    }
+
+    /* Perform test. */
+    mram_range range = { 0, host_to_dpu.length };
+    // base_sort_algo * const algo = algos[host_to_dpu.algo_index].data.fct;
+    memset(&dpu_to_host, 0, sizeof dpu_to_host);
+
+    for (uint32_t rep = 0; rep < host_to_dpu.reps; rep++) {
+        pivot_rngs[me()] = seed_xs_offset(host_to_dpu.basic_seed + me());
+
+        array_stats stats_before;
+        get_stats_unsorted(input, cache, range, false, &stats_before);
+
+        perfcounter_config(COUNT_CYCLES, true);
+        time new_time = perfcounter_get();
+        // algo(&input[range.start], &input[range.end - 1]);
+        merge_sort(&input[range.start], &input[range.end - 1]);
+        new_time = perfcounter_get() - new_time - CALL_OVERHEAD;
+        dpu_to_host.firsts += new_time;
+        dpu_to_host.seconds += new_time * new_time;
+
+        array_stats stats_after;
+        get_stats_sorted(input, cache, range, false, &stats_after);
+        if (compare_stats(&stats_before, &stats_after, false) == EXIT_FAILURE) {
+            abort();
+        }
+
+        range.start += host_to_dpu.offset;
+        range.end += host_to_dpu.offset;
+        host_to_dpu.basic_seed += NR_TASKLETS;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ dpu/wram_sorts.h
@@ -0,0 +1,144 @@
+/**
+ * @file
+ * @brief An implementation of the fastest WRAM QuickSort.
+**/
+
+#ifndef _BASE_SORT_H_
+#define _BASE_SORT_H_
+
+#include "common.h"
+#include "pivot.h"
+
+static __attribute__((unused)) T *call_stacks[NR_TASKLETS][40];  // call stack for iter. QuickSort
+
+/**
+ * @brief An implementation of standard InsertionSort.
+ * @attention This algorithm relies on `start[-1]` being a sentinel value,
+ * i.e. being at least as small as any value in the array.
+ * For this reason, `cache[-1]` is set to `T_MIN`.
+ * For QuickSort, the last value of the previous partition takes on that role.
+ * 
+ * @param start The first element of the WRAM array to sort.
+ * @param end The last element of said array.
+**/
+static void insertion_sort_wram(T * const start, T * const end) {
+    T *curr, *i = start + 1;
+    while ((curr = i++) <= end) {
+        T const to_sort = *curr;
+        while (*(curr - 1) > to_sort) {  // `-1` always valid due to the sentinel value
+            *curr = *(curr - 1);
+            curr--;
+        }
+        *curr = to_sort;
+    }
+}
+
+/* Defining building blocks for QuickSort, which remain the same. */
+// The main body of QuickSort remains the same no matter the implementation variant.
+#define QUICK_BODY()                                         \
+T * const pivot = get_pivot(left, right);                    \
+T const pivot_value = *pivot;                                \
+swap(pivot, right);  /* The pivot acts as sentinel value. */ \
+T *i = left - 1, *j = right;                                 \
+while (true) {                                               \
+    while (*++i < pivot_value);                              \
+    while (*--j > pivot_value);                              \
+    if (i >= j) break;                                       \
+    swap(i, j);                                              \
+}                                                            \
+swap(i, right)
+
+#define QUICK_GET_SHORTER_PARTITION() (!__RECURSIVE__)
+
+// Whether the partition has a length below the threshold.
+#define QUICK_IS_THRESHOLD_UNDERCUT() (right - left + 1 <= QUICK_THRESHOLD)
+
+#define QUICK_FALLBACK() insertion_sort_wram(left, right)
+
+#define QUICK_IS_TRIVIAL_LEFT() (i - 1 <= left)
+#define QUICK_IS_TRIVIAL_RIGHT() (right <= i + 1)
+
+#ifdef UINT64  // recursive variant
+
+#define __RECURSIVE__ (true)
+
+// Sets up `left` and `right` as synonyms for `start` and `end`.
+// They are distinct only in the iterative variant.
+#define QUICK_HEAD() T *left = start, *right = end
+
+// Unneeded for the recursive variant.
+#define QUICK_TAIL()
+
+// Obviously, ending the current QuickSort is done via `return`.
+#define QUICK_STOP() return
+
+#define QUICK_CALL_LEFT(name) name(left, i - 1)
+#define QUICK_CALL_RIGHT(name) name(i + 1, right)
+
+#else  // UINT64
+
+#define __RECURSIVE__ (false)
+
+// A “call” stack for holding the values of `left` and `right` is maintained.
+// Its memory must be reserved beforehand.
+#define QUICK_HEAD()                                    \
+T ** const start_of_call_stack = &call_stacks[me()][0]; \
+T **call_stack = start_of_call_stack;                   \
+*call_stack++ = start;                                  \
+*call_stack++ = end;                                    \
+do {                                                    \
+    T *right = *--call_stack, *left = *--call_stack
+
+// Closing the loop which pops from the stack.
+#define QUICK_TAIL() } while (call_stack != start_of_call_stack)
+
+// Obviously, ending the current QuickSort is done via `continue`.
+#define QUICK_STOP() continue
+
+#define QUICK_CALL_LEFT(name) do { *call_stack++ = left; *call_stack++ = i - 1; } while (false)
+#define QUICK_CALL_RIGHT(name) do { *call_stack++ = i + 1; *call_stack++ = right; } while (false)
+
+#endif  // UINT64
+
+/**
+ * @brief A fast implementation of QuickSort
+ * based on `quick_sort_check_trivial_before_call` from `benchmark/quick_sorts.c`.
+ * 
+ * @param start The first element of the WRAM array to sort.
+ * @param end The last element of said array.
+ */
+static void quick_sort_wram(T * const start, T * const end) {
+    QUICK_HEAD();
+    if (QUICK_IS_THRESHOLD_UNDERCUT()) {
+        QUICK_FALLBACK();
+        QUICK_STOP();
+    }
+    QUICK_BODY();
+    if (QUICK_GET_SHORTER_PARTITION()) {
+        if (!QUICK_IS_TRIVIAL_LEFT())
+            QUICK_CALL_LEFT(quick_sort);
+        if (!QUICK_IS_TRIVIAL_RIGHT())
+            QUICK_CALL_RIGHT(quick_sort);
+    } else {
+        if (!QUICK_IS_TRIVIAL_RIGHT())
+            QUICK_CALL_RIGHT(quick_sort);
+        if (!QUICK_IS_TRIVIAL_LEFT())
+            QUICK_CALL_LEFT(quick_sort);
+    }
+    QUICK_TAIL();
+}
+
+#undef QUICK_BODY
+#undef QUICK_GET_SHORTER_PARTITION
+#undef QUICK_IS_THRESHOLD_UNDERCUT
+#undef QUICK_FALLBACK
+#undef QUICK_IS_TRIVIAL_LEFT
+#undef QUICK_IS_TRIVIAL_RIGHT
+#undef __RECURSIVE__
+#undef QUICK_HEAD
+#undef QUICK_TAIL
+#undef QUICK_STOP
+#undef QUICK_CALL_LEFT
+#undef QUICK_CALL_RIGHT
+
+#endif  // _BASE_SORT_H_
