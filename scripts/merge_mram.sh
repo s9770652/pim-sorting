16feb0151f33ac430fc27c80d4b9d3d8b4526df2
s9770652 <s9770652@rz.uni-frankfurt.de>
2024-09-12 04:52:40 +0200

    Automated testing for MRAM MergeSort.

---
+++ scripts/merge_mram.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+b=7
+r=10
+n="0x7FFF7"
+
+main_folder=scripts/merge_mram
+mkdir -p ${main_folder}
+
+for nr_tasklets in 11 12 16
+do
+    tasklets_folder=${main_folder}/NR_TASKLETS=${nr_tasklets}
+    for cache_size in 256 512 1024
+    do
+        cache_folder=${tasklets_folder}/CACHE_SIZE=${cache_size}
+        for seqread_cache_size in 128 256 512
+        do
+            seqread_folder=${cache_folder}/SEQREAD_CACHE_SIZE=${seqread_cache_size}
+            for type in 32 64
+            do
+                make clean
+                NR_TASKLETS=${nr_tasklets} CACHE_SIZE=${cache_size} SEQREAD_CACHE_SIZE=${seqread_cache_size} TYPE=UINT${type} CHECK_SANITY=false make all
+
+                folder=${seqread_folder}/uint${type}
+                mkdir -p ${folder}
+
+                dists=("sorted" "reverse" "almost" "uniform" "zipf" "normal")
+                for dist in "${!dists[@]}";
+                do
+                    bin/host -b ${b} -r ${r} -t ${dist} -n ${nr_tasklets * n} | tee ${folder}/${dists[${dist}]}.txt
+                done
+            done
+        done
+    done
+done
